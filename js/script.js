31febce41261d76741b3fdcddea41854dacc9586
Luís <105549974+luisfjs14@users.noreply.github.com>
2025-06-17 21:00:29 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.getElementById('menuToggle');
+  const navMenu = document.getElementById('navMenu');
+
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+});
+
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) slide.classList.add("active");
+  });
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  showSlide(currentIndex);
+}
+
+// Auto slide
+setInterval(() => {
+  nextSlide();
+}, 5000); // muda a cada 5 segundos
